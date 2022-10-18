e35688386533ef750a7551d0cfa003179d295255
Andrew Cassidy <andee@bytz.co.uk>
2022-10-18 16:40:58 +0100

    2015 day 8

---
+++ 2015/day8.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import re
+
+HEX_REGEX = re.compile(r'\\x[0-9a-fA-F]{2}')
+
+with open('day8input.txt', 'r') as f:
+    # Newlines are your enemy in this task
+    lines = [x.strip() for x in f.readlines()]
+
+    # Part 1 is fairly straightforward.
+    stored = sum([len(x) for x in lines])
+
+    # And decode the stored forms using the dead simple rules given.
+    literal = sum([len(
+        HEX_REGEX.sub("'", x[1:-1].replace("\\\\", "\\").replace("\\\"", "\""))) for x in lines
+    ])
+
+    # Part 2 is to double-encode the already encoded
+    double = sum([
+        # DO THE SLASHES FIRST. Otherwise, it then doubles up the escapes for the quotes.
+        len("\"" + x.replace("\\", "\\\\").replace("\"", "\\\"") + "\"")
+        for x in lines])
+
+    print(stored - literal)
+    print(double - stored)
